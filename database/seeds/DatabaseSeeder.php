249ee5696a3903563b8225f4446e750f04731819
William <wtpwinberg@gmail.com>
2019-07-25 22:06:27 -0600

    db instantiated

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'password' => Hash::make('admin'),
+            'role' => 2
+        ]);        User::create([
+        'name' => 'User',
+        'email' => 'user@test.com',
+        'password' => Hash::make('secret'),
+        'role' => 1
+    ]);
     }
 }
