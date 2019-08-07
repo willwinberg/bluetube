43ef6cfb79e9852abf0cc4338c9295c67f27cff4
William <wtpwinberg@gmail.com>
2019-08-06 23:54:02 -0600

    set upp vue init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
-/**
- * First, we will load all of this project's Javascript utilities and other
- * dependencies. Then, we will be ready to develop a robust and powerful
- * application frontend using useful Laravel and JavaScript libraries.
- */
+import './bootstrap';
+import Vue from 'vue';
+import Vuetify from 'vuetify;'
 
-require('./bootstrap');
+import Routes from '@/js/routes.js';
+import App from '@/views/App';
+
+Vue.use(Vuetify);
+
+const app = new Vue({
+    el: '#app',
+    router: Routes,
+    render: h => h(App)
+});
+
+export default app;
+
