38f2d5e2738d332fca3ddd5f49b0d7a741d7c9ba
William <wtpwinberg@gmail.com>
2019-08-06 23:45:00 -0600

    deleted template file for vue SAP

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Auth::routes();
+//
+//Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/{any}', 'SinglePageController@index')->where('any', '.*');
